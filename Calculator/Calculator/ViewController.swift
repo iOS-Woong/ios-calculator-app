b3040994e5b2db5d679ca4d0af14466f43b9c83b
woong <workplayhard1@naver.com>
2022-09-30 18:40:00 +0900

    feat: 숫자입력시 입력라벨에 숫자가 표시되도록하는 액션함수 tappedNumberPad 함수 구현(+첫번째값 입력 예외처리 포함)

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,11 +7,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let defaultZeroValue = "0"
+    var isEnabledTyping: Bool = false
+    
+    @IBOutlet weak var numberPadZero: UIButton!
+    @IBOutlet weak var numberPadZeroZero: UIButton!
+    @IBOutlet weak var nowInputValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nowInputValue.text = defaultZeroValue
     }
-
-
+        
+    @IBAction func tappedNumberPad(_ sender: UIButton) {
+        guard let button = sender.titleLabel?.text else { return }
+        guard let checkNowInputValue = nowInputValue else { return }
+        
+        if isEnabledTyping == true {
+            checkNowInputValue.text! += button
+        } else {
+            checkNowInputValue.text = button
+            isEnabledTyping = true
+        }
+    }
 }
-
